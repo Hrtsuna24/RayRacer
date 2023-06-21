a2d3ec9584fb25259be52afd577d0f4825e237f9
Hrtsuna24 <93074808+Hrtsuna24@users.noreply.github.com>
2023-06-21 15:17:56 +0300

    Materials

--- ShitRacer/src/Renderer.cpp
+++ ShitRacer/src/Renderer.cpp
@@ -1,5 +1,5 @@
 #include "Renderer.h"
-
+#include "Walnut/Random.h"
 
 namespace Utils
 {
@@ -156,13 +156,13 @@ glm::vec4 HTM::Renderer::PerPixel(uint32_t x, uint32_t y)
 	glm::vec3 color(0.f);
 	float multiplier = 1.0f;
 
-	size_t bounces = 2;
+	size_t bounces = 5;
 	for (size_t i = 0; i < bounces; i++)
 	{
 		HitPayload payload = TraceRay(ray);
 		if (payload.HitDistance < 0.f)
 		{
-			glm::vec3 skyColor = glm::vec3(0.f, 0.f, 0.f);
+			glm::vec3 skyColor = glm::vec3(0.6f, 0.7f, 0.9f);
 			color += skyColor * multiplier;
 			break;
 		}
@@ -171,15 +171,17 @@ glm::vec4 HTM::Renderer::PerPixel(uint32_t x, uint32_t y)
 		float d = glm::max(glm::dot(payload.WorldNormal, -lightDirecton), 0.0f);// cos(angel) between n and lD
 
 		const Sphere& sphere = m_Scene->spheres[payload.ObjectIndex];
+		const Material& material = m_Scene->Materials[sphere.MaterialIndex];
 
-		glm::vec3 sc = sphere.Albedo;
+		glm::vec3 sc = material.Albedo;
 		sc *= d;
 		color += sc * multiplier;
 
-		multiplier *= 0.7f;
+		multiplier *= 0.5f;
 
 		ray.Origin = payload.WorldPosition + payload.WorldNormal * 0.0001f;
-		ray.Direction = glm::reflect(ray.Direction, payload.WorldNormal);
+		ray.Direction = glm::reflect(ray.Direction,
+			payload.WorldNormal + material.Roughness * Walnut::Random::Vec3(-0.5f, 0.5f) );
 	}
 	return glm::vec4(color, 1.f);
 }
--- ShitRacer/src/Scene.h
+++ ShitRacer/src/Scene.h
@@ -2,16 +2,27 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <vector>
 
+
+struct Material 
+{
+
+	glm::vec3 Albedo{ 1.f };
+	float Roughness{ 1.f };
+	float Metallic{ 0.f };
+};
+
+
 struct Sphere
 {
 	glm::vec3 Position{ 0.f };
 	float Radius{ 0.5 };
 
 
-	glm::vec3 Albedo{ 1.f };
+	size_t MaterialIndex = 0;
 };
 
 struct Scene
 {
 	std::vector<Sphere> spheres;
+	std::vector<Material> Materials;
 };
